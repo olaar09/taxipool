7dde3259924dec5327503f9df58592a7ff8c5177
Agboola Yusuf <agboolar09@gmail.com>
2018-02-25 09:45:02 +0100

    ..

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var baseUrl = 'http://127.0.0.1:8000';
 var findpoolUrl = '/findpool';
 
+
+var locations = {
+    destination : null,
+    pickup : null
+}
+
 function makeRequestGet(url, successCallBack, errorCallBack) {
     $.ajax({
         url: url,
@@ -36,4 +42,46 @@ function afterFind(data) {
 
 function findFailed() {
     console.log(data)
+}
+
+
+function setPickLocation() {
+    googleApiAutoComplete('pickup_area', function(place){
+        locations.pickup = place.id;
+    });
+}
+
+function setDestination(){
+    googleApiAutoComplete('destination_area', function(place){
+        locations.destination = place.id;
+    });
+}
+
+function googleApiAutoComplete(formId, callBack) {
+
+    var input = document.getElementById(formId);
+    var options = {
+        //strictBounds: true,
+        types: ['geocode']
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+
+    autocomplete.addListener('place_changed', function () {
+        callBack(autocomplete.getPlace());
+        console.log(autocomplete.getPlace());
+    });
 }
